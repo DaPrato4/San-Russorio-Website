002dd39bb7034f69c1a2822af6e31f7f78d19345
DaPrato4 <daprato363@gmail.com>
2025-04-03 17:36:40 +0200

    init footer

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!-- PAGE HEADER -->
  
 <h1 class="bg-black text-white p-4">Prova tema N° 00</h1>
-<div class="bg-blue-800 mx-auto rounded-lg shadow-gray-400 shadow-lg p-4 w-1/2 text-center text-white text-xl mt-4 transform transition-transform duration-300 
+<div class="bg-blue-800 mx-auto rounded-lg shadow-gray-400 shadow-lg p-4 w-1/2 text-center text-white text-xl mt-4 mb-64 transform transition-transform duration-300 
             hover:translate-y-2 hover:translate-x-2 cursor-pointer">
     questa è una div di prova di tailwindcss....
 </div>
