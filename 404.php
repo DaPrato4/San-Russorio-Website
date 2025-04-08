25e8cc8203076257af95cfa5fc6d5ed90ccb3cf7
DaPrato4 <daprato363@gmail.com>
2025-04-08 14:13:05 +0200

    completed 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,14 @@
 <?php get_header() ?>
 <!-- PAGE HEADER -->
 
-<h1>page not found 404</h1>
+<div class="h-screen flex items-center justify-center bg-secondary">
+    <div class="text-center">
+        <h1 class="text-4xl mb-4">Pagina non trovata</h1>
+        <h4 class="mb-7">404 - La pagina richiesta non è stata trovata</h4>
+
+        <a class="bg-primary text-amber-50 py-3 px-6 rounded-lg text-xl" href="/">Torna alla Homepage</a>
+    </div>
+</div>
 
 
 <?php get_footer() ?>
